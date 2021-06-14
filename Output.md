017ff87a338fd4f3665ada4b9fabded6fe398128
Matt Winter <winter5187@gmail.com>
2021-06-13 21:09:05 -0500

    hw9 js updates

--- Output.md
+++ Output.md
@@ -1,1 +1,6 @@
-Title:Readme
+#Title: 1
+    The description is 2
+    The installation instructions are 3
+    The usage information is 4
+    The contribution guidelines are 5
+    The test instructions are 6
--- index.js
+++ index.js
@@ -8,12 +8,42 @@ inquirer
       name: 'title',
       message: 'What is the title?',
     },
-
+    {
+    type: 'input',
+    name: 'description',
+    message: 'What is the description?'
+    },
+    {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions?'
+    },
+    {
+    type: 'input',
+    name: 'usage',
+    message: 'What is the usage information?'
+    },
+    {
+    type: 'input',
+    name: 'contribution',
+    message: 'What are the contribution guidelines?'
+    },
+    {
+    type: 'input',
+    name: 'test', 
+    message: 'What are the test instructions?'
+    }
+    
     
   ])
   .then((data) => {
     console.log(data);
-    let README = `#Title: ${data.title}`;
+    let README = `#Title: ${data.title}
+    The description is ${data.description}
+    The installation instructions are ${data.installation}
+    The usage information is ${data.usage}
+    The contribution guidelines are ${data.contribution}
+    The test instructions are ${data.test}`;
     fs.writeFileSync('Output.md', README);
   });
 
